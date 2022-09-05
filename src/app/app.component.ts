20f18976089a3c18c32d9858c93bea4f4faf6284
Laura C. Barros <lauracbarros@hotmail.com>
2022-09-04 22:54:59 -0300

    criação layout basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project-html-css-gama-academy';
+  title = 'project-html-css-gama-academy'; livros = [
+    { "nome":"teste Objeto", "descr":"teste desc", "autor":"teste autor", "editora":"teste editora" },
+    { "nome":"teste Objeto 2", "descr":"teste desc2", "autor":"teste autor2", "editora":"teste editora2" },
+    { "nome":"teste Objeto3", "descr":"teste desc3", "autor":"teste autor3", "editora":"teste editora3" }
+  ]
 }
+
+
